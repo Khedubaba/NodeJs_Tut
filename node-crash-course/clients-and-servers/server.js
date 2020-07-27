8198cd36a1db0b159dabaf71bc30b9d0609ef250
khedubaba <adityakhedekar49@gmail.com>
2020-07-27 08:25:44 +0530

    express added

--- node-crash-course/clients-and-servers/server.js
+++ node-crash-course/clients-and-servers/server.js
@@ -1,61 +1,63 @@
-const http = require('http');
-const fs = require('fs');
-const _ = require('lodash');
+//Replaced this server.js with app.js which uses express framework.
 
-const server = http.createServer((req, res) => {
+// const http = require('http');
+// const fs = require('fs');
+// const _ = require('lodash');
+
+// const server = http.createServer((req, res) => {
     
-    //lodash
-    const num = _.random(0, 20);
-    console.log(num);
+//     //lodash
+//     const num = _.random(0, 20);
+//     console.log(num);
 
-    const greet = _.once(() => {
-        console.log('hello');
-    });
+//     const greet = _.once(() => {
+//         console.log('hello');
+//     });
 
-    greet();
-    greet();
+//     greet();
+//     greet();
 
-    //set header content type
-    res.setHeader('Content-Type', 'text/html');
+//     //set header content type
+//     res.setHeader('Content-Type', 'text/html');
 
-    // res.write('<p>Hello, everyone</p>');
-    // res.write('<p>Hello again! everyone</p>');
-    // res.end();
+//     // res.write('<p>Hello, everyone</p>');
+//     // res.write('<p>Hello again! everyone</p>');
+//     // res.end();
 
-    let path = './views/';
-    switch(req.url) {
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        case '/about-us':
-            res.statusCode = 301;
-            res.setHeader('Location', '/about');
-            res.end();
-            break;    
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
-    }
+//     let path = './views/';
+//     switch(req.url) {
+//         case '/':
+//             path += 'index.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             path += 'about.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about-us':
+//             res.statusCode = 301;
+//             res.setHeader('Location', '/about');
+//             res.end();
+//             break;    
+//         default:
+//             path += '404.html';
+//             res.statusCode = 404;
+//             break;
+//     }
 
-    //Send an html file
-    fs.readFile(path, (err,data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            //res.write(data);
-            res.end(data);
-        }
-    })
+//     //Send an html file
+//     fs.readFile(path, (err,data) => {
+//         if (err) {
+//             console.log(err);
+//             res.end();
+//         } else {
+//             //res.write(data);
+//             res.end(data);
+//         }
+//     })
 
-});
+// });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening on port 3000');
-});
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening on port 3000');
+// });
